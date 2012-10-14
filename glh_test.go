d7a4c7bd8e1b3d7af7cb8f96e7a004250c0898a8
Peter Waller <peter@pwaller.net>
2012-10-14 12:44:34 +0100

    tests: Move some things out to go-gl/testutils

--- glh_test.go
+++ glh_test.go
@@ -1,80 +1,16 @@
 package glhelpers
 
 import (
-	"log"
-	"runtime"
 	"testing"
 
 	"github.com/go-gl/gl"
-	"github.com/go-gl/glfw"
+	"github.com/go-gl/testutils"
 )
-
-var main_thread_work = make(chan func())
-var main_thread_work_done = make(chan bool)
-
-func OnTheMainThread(setup, after func()) {
-	main_thread_work <- setup
-	main_thread_work <- after
-	<-main_thread_work_done
-}
-
-func init() {
-	go func() {
-		runtime.LockOSThread()
-
-		if err := glfw.Init(); err != nil {
-			log.Panic("glfw Error:", err)
-		}
-
-		w, h := 100, 100
-		err := glfw.OpenWindow(w, h, 0, 0, 0, 0, 0, 0, glfw.Windowed)
-		if err != nil {
-			log.Panic("Error:", err)
-		}
-
-		if gl.Init() != 0 {
-			log.Panic("gl error")
-		}
-
-		glfw.SetWindowSizeCallback(Reshape)
-		glfw.SwapBuffers()
-
-		for {
-			(<-main_thread_work)()
-			glfw.SwapBuffers()
-			(<-main_thread_work)()
-			main_thread_work_done <- true
-		}
-
-	}()
-}
-
-func SetWindowSize(width, height int) {
-	glfw.SetWindowSize(width, height)
-	// Need to wait for the reshape event, otherwise it happens at an arbitrary
-	// point in the future (some unknown number of SwapBuffers())
-	glfw.WaitEvents()
-}
-
-func Reshape(width, height int) {
-	gl.Viewport(0, 0, width, height)
-
-	gl.MatrixMode(gl.PROJECTION)
-	gl.LoadIdentity()
-	gl.Ortho(-1, 1, -1, 1, -1, 1)
-	//gl.Ortho(-2.1, 6.1, -2.25*2, 2.1*2, -1, 1) // Y debug
-
-	gl.MatrixMode(gl.MODELVIEW)
-	gl.LoadIdentity()
-
-	gl.ClearColor(0, 0, 0, 1)
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-}
 
 // Draw a test pattern
 func TestWindowCoords(t *testing.T) {
-	OnTheMainThread(func() {
-		SetWindowSize(40, 40)
+	gltest.OnTheMainThread(func() {
+		gltest.SetWindowSize(40, 40)
 
 		w, h := GetViewportWH()
 		With(WindowCoords{}, func() {
