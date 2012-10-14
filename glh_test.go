c913b5c44fda06f03e4d560367a56295baa79676
Peter Waller <peter@pwaller.net>
2012-10-14 14:32:14 +0100

    Some test updates: fill some additional pixels

--- glh_test.go
+++ glh_test.go
@@ -17,6 +17,7 @@ func TestWindowCoords(t *testing.T) {
 			// So that we draw in the middle of the pixel
 			gl.Translated(0.5, 0.5, 0)
 
+			// Draw stripes
 			stride := 1
 			internal_n := 4
 			for b := 0; b < w/2-internal_n*stride; b += stride {
@@ -58,12 +59,16 @@ func TestWindowCoords(t *testing.T) {
 				gl.Vertex2i(w/2-4, h)
 			})
 
-			// Remove top left and top right pixel
+			// Remove some pixels near the boundaries
 			gl.PointSize(1)
 			gl.Color4f(0, 0, 0, 1)
 			With(Primitive{gl.POINTS}, func() {
+				// Black dot in top left
 				gl.Vertex2i(0, 0)
+				// Off the top right (should not be visible)
 				gl.Vertex2i(w, 0)
+				// Bottom left pixel (should be visible as a black dot)
+				gl.Vertex2i(0, h-1)
 			})
 		})
 	}, func() {
@@ -74,13 +79,14 @@ func TestWindowCoords(t *testing.T) {
 // Draw a test pattern
 func TestWindowCoordsA(t *testing.T) {
 	gltest.OnTheMainThread(func() {
-		gltest.SetWindowSize(40, 40)
+		gltest.SetWindowSize(40, 5)
 
 		w, h := GetViewportWH()
 		With(WindowCoords{}, func() {
 			// So that we draw in the middle of the pixel
 			gl.Translated(0.5, 0.5, 0)
 
+			gl.Color4f(1, 1, 1, 1)
 			With(Primitive{gl.POINTS}, func() {
 				for i := 0; i < w+1; i += 2 {
 					gl.Vertex2i(i, h/2)
